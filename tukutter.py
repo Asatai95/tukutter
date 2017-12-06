328688473cce95571db622cb9b2e6681e19b36e5
Asatai95 <defense433@gmail.com>
2017-12-06 12:31:25 +0000

    tukutter.py 追加

--- tukutter.py
+++ tukutter.py
@@ -1,5 +1,5 @@
 import MySQLdb
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, make_response
 
 application = Flask(__name__)
 
@@ -7,6 +7,15 @@ application = Flask(__name__)
 def top():
 
     return render_template('top.html')
+
+# @application.route('/login')
+# def cookie():
+#
+#     resp = make_response('cookie set')
+#     resp.set_cookie('log_id', 'passwd')
+#
+#     print('resp')
+
 
 @application.route('/login')
 def login():
@@ -18,24 +27,38 @@ def login_db():
 
     log_id = request.form['log_id']
     passwd = request.form['passwd']
+    print(log_id)
+    print(passwd)
 
     db = MySQLdb.connect( user='root', passwd='asatai95',
         host='localhost', db='tukutter', charset='utf8')
+    print('???')
 
     con = db.cursor()
+    print(con)
 
-    sql = "select log_id, passwd from users where log_id and passwd =' " +log_id+ " ' and ' " +passwd+ " ' "
+    sql = "select log_id, passwd from users where log_id = '" + log_id + "' and passwd = '" + passwd + "'"
     con.execute(sql)
-    passwd = con.fetchall()
+    result = con.fetchall()
+    print(result)
+    print(sql)
 
     if len(log_id) and len(passwd) is 0:
 
-         return render_template('top.html')
+        print('top.html')
+
+        return render_template('top.html')
 
     else:
-         return redirect('http://localhost:8080/login')
+        print('login.html')
+        return redirect('http://localhost:8080/login')
 
     print(result)
+
+@application.route('/logout')
+def logout():
+
+    return redirect('https://localhost:8080/login')
 
 @application.route('/new')
 def new():
